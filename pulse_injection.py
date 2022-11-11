491ecda8ec5c613937acc55a7dfe634698b469e8
David Konijn <74962121+DavidKonijn@users.noreply.github.com>
2022-11-11 10:22:44 +0100

    Updates pulse injection
    Updated the pulse object to be a complete Gaussian pulse

--- pulse_injection.py
+++ pulse_injection.py
@@ -7,8 +7,6 @@ import os
 import argparse
 import glob
 
-from jess.fitters import median_fitter
-from scipy.stats import median_abs_deviation
 from will import create, inject
 from your import Your
 from your.formats.filwriter import make_sigproc_object
@@ -16,13 +14,22 @@ from your.formats.filwriter import make_sigproc_object
 parser = argparse.ArgumentParser(description="""Inject a FRB-like pulse into the input filterbank data and store the h5 file.""")
 parser.add_argument('-t','--FRBtime',type=float,required=True)
 parser.add_argument('-f','--FRBfreq',type=float,required=True)
-parser.add_argument('-l','--lilo',type=str,required=True)
-parser.add_argument('-d','--dm',type=int,required=True)
-parsed.add_argument('-snr','--snr', type=float,required=True)
+parser.add_argument('-l','--FRBlocation',type=float,required=True)
+parser.add_argument('-n','--name',type=str,required=True)
+parser.add_argument('-d','--dm',type=float,required=True)
+parser.add_argument('-snr','--snr',type=float,required=True)
+parser.add_argument('-scints','--FRBscint',type=int,required=True)
+
 args = parser.parse_args()
 
-os.chdir("../inject_pulse")
-lilo = glob.glob("../data_files/"+args.lilo +"*")[0]
+try:
+    os.system('rm -r inject_pulse')
+except:
+    print("There is no inject_pulse file")
+
+os.system('mkdir inject_pulse')
+os.chdir("inject_pulse")
+lilo = glob.glob("../data_files/"+args.name+"*")[0]
 print(lilo)
 
 empty_fil =  lilo
@@ -30,7 +37,7 @@ yr_obj = Your(empty_fil)
 
 dm = args.dm
 
-if dm >= 170:
+if dm >= 500:
     # calculate the dispersive time delay
     f1 = yr_obj.your_header.center_freq - abs(yr_obj.your_header.bw)/2
     f2 = yr_obj.your_header.center_freq + abs(yr_obj.your_header.bw)/2
@@ -39,22 +46,26 @@ if dm >= 170:
     # the entire generated-filterbank time will be 5 times the dispersive delay
     dispersive_range = int(((dispersive_time_delay/1000)/yr_obj.your_header.tsamp)*5)
 else:
-    dispersive_range = 75000
+    dispersive_range = 200000
 
-pulse_obj = create.SimpleGaussPulse(
-            sigma_time=args.FRBtime,
-                sigma_freq=args.FRBfreq,
-                    dm=args.dm,
-                        center_freq=yr_obj.your_header.center_freq,
-                            tau=20,
-                                phi=np.pi / 3,
-                                    spectral_index_alpha=0,
-                                        chan_freqs=yr_obj.chan_freqs,
-                                            tsamp=yr_obj.your_header.tsamp,
-                                                nscint=0,
-                                                    )
+# create the pulse object using general and acquited parameters
+pulse_obj = create.GaussPulse(
+    sigma_times=args.FRBtime,
+    sigma_freqs=args.FRBfreq,
+    offsets=0,
+    pulse_thetas=0,
+    relative_intensities=1,
+    dm=args.dm,
+    center_freqs=yr_obj.your_header.center_freq-args.FRBlocation,
+    tau=20,
+    phi=np.pi / 3,
+    spectral_index_alpha=0,
+    chan_freqs=yr_obj.chan_freqs,
+    tsamp=yr_obj.your_header.tsamp,
+    nscint=args.FRBscint,
+)
 
-pulse = pulse_obj.sample_pulse(nsamp=int(3e5))
+pulse = pulse_obj.sample_pulse(nsamp=int(3e5)) * args.snr
 
 # choose a random location in noise, with enough space for 3000dm pulse
 i = np.random.randint(0, int(0.8*yr_obj.your_header.nspectra),1)[0]
@@ -64,12 +75,12 @@ j = np.random.randint(0, int(0.8*dispersive_range),1)[0]
 print("Location of noise:", i)
 print("Location of start:", j)
 
-#inject the generated pulse object into the noise filterbank
+# inject the generated pulse object into the noise filterbank
 dynamic_spectra_w_pulse = inject.inject_constant_into_file(
             yr_input=yr_obj,
-                pulse=pulse*args.snr,
+                pulse=(pulse*args.snr).astype('uint64'),
                     start=i+j,
-                        gulp = 5000000,
+                        gulp=5000000,
                         )
 
 # initialize the new .fil file to turn into candidate .h5
@@ -92,7 +103,7 @@ os.system( "scp ../*.py .")
 
 # create the identify.sh file to identify the badchannels infected by RFI
 txtfile = open('identify.sh', 'w')
-txtfile.write('python identify_badchans_py3.py -l '+str(dm-50)+' -d '+str(dm+50)+' -f injected_pulse.fil')
+txtfile.write('python identify_badchans_py3.py -l '+str(int(dm)-30)+' -d '+str(int(dm)+30)+' -f injected_pulse.fil')
 txtfile.close()
 
 # zap the identified channels and make the .h5 candidate
@@ -101,18 +112,8 @@ os.system( "bash summonheimdall.sh")
 
 # create the heimcsv_line.sh file to add the low changable dm
 txtfile = open('heimcsv_line.sh', 'w')
-txtfile.write( "python heimcsv.py -p /data/konijn/eclat/pulse_injection/inject_pulse -d "+str(dm))
+txtfile.write( "python heimcsv.py -p /data/konijn/eclat/pulse_injection/inject_pulse -d "+str(int(dm))+" -t "+str(j))
 txtfile.close()
 
 os.system( "bash heimcsv_line.sh")
 os.system( "your_candmaker.py -o ../h5_candidates -n 32 -c phoenix.csv")
-
-# copy the h5 candidate and reset the folder
-# snr_name = glob.glob("*.h5")[0][-11:][:8]
-# os.system( "mv injected_pulse.fil injected_pulse_"+snr_name+".fil")
-# os.system( "scp injected_pulse.fil ../h5_candidates")
-# os.system( "scp *.h5 ../h5_candidates")
-
-# os.chdir( ".." )
-# os.system( "rm -r inject_pulse")
-# os.system( "mkdir inject_pulse")
